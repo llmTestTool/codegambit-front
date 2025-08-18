6b80bd428edca6534c904b65a9d7e8620a51399b
philosophia301 <eunjong147@gmail.com>
2025-08-18 23:51:46 +0900

    feat: report page

--- src/route/MainRoutes.tsx
+++ src/route/MainRoutes.tsx
@@ -1,22 +1,25 @@
 import { useRoutes } from "react-router-dom";
 
-import MainLayout from '../layout/MainLayout';
 import DashBoardPage from "@page/applicant/DashBoardPage";
-import AssignListPage from '../page/AssignList/AssignListPage';
+import MainLayout from '../layout/MainLayout';
 import AssignEditPage from '../page/AssignEditPage';
+import AssignListPage from '../page/AssignList/AssignListPage';
+import AssignViewPage from '../page/AssignViewPage';
+import ReportPage from "../page/report/ReportPage";
 
 
 const MainRoutes = () => {
   const routes = useRoutes([
     {
-
       path: "/", element: <MainLayout />,
       children: [
         { path: '/dashboard', element: <DashBoardPage />},
         { path: '/assignlist', element: <AssignListPage />},
         { path: '/assignedit', element: <AssignEditPage />},
+        { path: '/report/1', element: <ReportPage />},
+        { path: "/assign/1", element: <AssignViewPage />}
       ]
-    },
+    }
   ]);
 
   return routes;
